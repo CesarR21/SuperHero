da5a6c83256019aad6d7f2ca900bf2daa8a510a2
unknown <Bizcanna21@icloud.com>
2021-08-26 11:25:45 -0500

    updated index path and index templates

--- superhero_project/superheroes/views.py
+++ superhero_project/superheroes/views.py
@@ -1,3 +1,11 @@
+from .models import Superhero
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    all_heroes= Superhero.objects.all()
+    context= {
+        'all_heroes': all_heroes
+    }
+    return render(request, 'superheroes/index.html',context)
